1c2e913cc10e8773f56a62c171c74fa742d4ba6d
LAURENT Louis <louis_laurent@aol.fr>
2020-07-06 14:27:53 +0200

    remove unused function

--- src/main/java/fr/pala/accounting/model/UserModel.java
+++ src/main/java/fr/pala/accounting/model/UserModel.java
@@ -5,12 +5,18 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
+import fr.pala.accounting.serializer.UserJsonSerializer;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.util.Date;
 
 @Document(collection = "User")
 @Getter
 @Setter
+@JsonSerialize(using = UserJsonSerializer.class)
 public class UserModel {
 
     @Id
@@ -19,9 +25,9 @@ public class UserModel {
     private String email;
     private Date created;
     private Date last_connection;
-    private ArrayList<AccountModel> accounts;
+    private List<AccountModel> accounts;
 
-    public UserModel(String user_id, String name, String email, Date created, Date last_connection, ArrayList<AccountModel> accounts){
+    public UserModel(String user_id, String name, String email, Date created, Date last_connection, List<AccountModel> accounts){
         this.user_id = user_id;
         this.name = name;
         this.email = email;
