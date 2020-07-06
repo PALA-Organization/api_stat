107351ff317bb5ba269d8f881ce0115ea216c5a4
LAURENT Louis <louis_laurent@aol.fr>
2020-07-06 14:27:07 +0200

    modify DAO for use optional return

--- src/main/java/fr/pala/accounting/dao/TransactionDAO.java
+++ src/main/java/fr/pala/accounting/dao/TransactionDAO.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionDAO {
@@ -21,15 +21,15 @@ public class TransactionDAO {
     @Autowired
     private AccountDAO accountDAO;
 
-    public List<TransactionModel> getAllTransactionsOfAccount(String user_id, String account_id) {
+    public Optional<List<TransactionModel>> getAllTransactionsOfAccount(String user_id, String account_id) {
         List<TransactionModel> transactionResults = new ArrayList<TransactionModel>();
-        AccountModel accountModel = accountDAO.getAccountOfUser(user_id, account_id);
+        Optional<AccountModel> accountModel = accountDAO.getAccountOfUser(user_id, account_id);
 
-        if(accountModel == null){
-            return transactionResults;
+        if(accountModel.isEmpty()){
+            return Optional.empty();
         }
 
-        ArrayList<String> transactions_ids = accountModel.getTransactions_ids();
+        ArrayList<String> transactions_ids = accountModel.get().getTransactions_ids().toList();
 
         for (String transactions_id : transactions_ids) {
             Query query = new Query();
@@ -37,55 +37,12 @@ public class TransactionDAO {
             transactionResults.add(mongoTemplate.findOne(query, TransactionModel.class));
         }
 
-        return transactionResults;
+        return Optional.of(transactionResults);
     }
 
-    public TransactionModel getTransaction(String transaction_id) {
+    public Optional<TransactionModel> getTransaction(String transaction_id) {
         Query query = new Query();
         query.addCriteria(Criteria.where("transaction_id").is(transaction_id));
-        return mongoTemplate.findOne(query, TransactionModel.class);
-    }
-
-    public TransactionModel addTransaction(String user_id, String account_id, TransactionModel transactionModel) {
-        TransactionModel transactionResult = mongoTemplate.save(transactionModel);
-
-        //add transaction to account
-        AccountModel account = accountDAO.getAccountOfUser(user_id, account_id);
-        ArrayList<String> transactions_ids = account.getTransactions_ids();
-        transactions_ids.add(transactionResult.getTransaction_id());
-        account.setTransactions_ids(transactions_ids);
-        accountDAO.updateAccount(user_id, account_id, account);
-        return transactionResult;
-    }
-
-    public void updateTransaction(TransactionModel transactionModel) {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("transaction_id").is(transactionModel.getTransaction_id()));
-        Update update = new Update();
-        update.set("type", transactionModel.getType());
-        update.set("shop_name", transactionModel.getShop_name());
-        update.set("shop_address", transactionModel.getShop_address());
-        update.set("date", transactionModel.getDate());
-        update.set("amount", transactionModel.getAmount());
-        update.set("description", transactionModel.getDescription());
-
-        mongoTemplate.findAndModify(query, update, TransactionModel.class);
-    }
-
-    public void deleteTransaction(String user_id, String account_id, TransactionModel transaction) {
-
-        mongoTemplate.remove(transaction);
-
-        AccountModel account = accountDAO.getAccountOfUser(user_id, account_id);
-        ArrayList<String> transactions_ids = account.getTransactions_ids();
-
-        for (int i = 0; i < transactions_ids.size(); i++) {
-            if(transactions_ids.get(i).equals(transaction.getTransaction_id())){
-                transactions_ids.remove(i);
-                break;
-            }
-        }
-        account.setTransactions_ids(transactions_ids);
-        accountDAO.updateAccount(user_id, account_id, account);
+        return Optional.of(mongoTemplate.findOne(query, TransactionModel.class));
     }
 }
